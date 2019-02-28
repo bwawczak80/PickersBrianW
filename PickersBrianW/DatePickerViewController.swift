cdfcef903fb94aafec6882746da11ebc176b26f2
Brian Wawczak <bwawczak@students.mchenry.edu>
2019-02-27 19:46:40 -0600

    sound not yet working

---
+++ PickersBrianW/DatePickerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DatePickerViewController.swift
+//  PickersBrianW
+//
+//  Created by Brian Wawczak on 2/25/19.
+//  Copyright © 2019 Brian Wawczak. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated: false)
+    }
+    
+
+    @IBAction func onButtonPressed(_ sender: Any) {
+        
+        let date = datePicker.date
+        let message = "The date and time you selected is \(date)"
+        let alert = UIAlertController(
+        title: "Date and Time Selected", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "That's so true", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
